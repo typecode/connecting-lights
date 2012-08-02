68301bdc32257f93bdcb8f1b3b989a1c7737ea75
zach lieberman <zach@openframeworks.cc>
2012-08-02 05:40:57 -0400

    minor correction on length of hadrians wall

--- wp-content/themes/connecting-lights/homepage.php
+++ wp-content/themes/connecting-lights/homepage.php
@@ -14,7 +14,7 @@ get_header();
 			<div>
 	
 				<p>Connecting Light is a digital art installation along Hadrian&rsquo;s Wall World Heritage Site.</p>
-				<p>The installation consists of hundreds of large-scale, light-filled balloons transmitting colors from one-to-another, creating a communication network spanning over one-hundred miles.</p>
+				<p>The installation consists of hundreds of large-scale, light-filled balloons transmitting colors from one-to-another, creating a communication network spanning over seventy miles.</p>
 		
 				<div class="more">
 					<p>Audience members are invited to participate by sending personalized messages along the light- lined wall at a number of viewing locations or, this Web site and companion mobile app.</p>

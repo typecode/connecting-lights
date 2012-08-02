dcb73198df2fba26448e9a1a37dc2eb3f4314e8d
Tyler Madsen <tylrmden@gmail.com>
2012-08-02 11:29:50 -0400

    blog

--- wp-content/themes/connecting-lights/index.php
+++ wp-content/themes/connecting-lights/index.php
@@ -3,7 +3,35 @@ get_header();
 ?>
 
 	<div class="inverted">
+		<div class="inner">
+		
+			<header>
+				<h1>Blog</h1>
+			</header>
+			
+			<div>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
+				
+				<article>
+	
+					<header>
+						<h1><?php the_title(); ?></h1>
+					</header>
+		
+					<div class="content">
+						<?php the_content(); ?>
+					</div>
+					
+				</article>
+			
+			<?php endwhile; else: ?>
+			<?php endif; ?>
+			</div>
+		</div>
+	</div>
 
+	<div class="xFull">
+		<?php require_once(TEMPLATEPATH . "/incl/actions.php"); ?>
 	</div>
 
 <?php

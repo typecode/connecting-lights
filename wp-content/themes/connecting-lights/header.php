39f7c52eabfa4f7df42ed2b47cb4fdef13f5f7a2
zach lieberman <zach@eyebeam.org>
2012-08-02 23:49:58 +0000

    john's working on the header...

--- wp-content/themes/connecting-lights/header.php
+++ wp-content/themes/connecting-lights/header.php
@@ -35,6 +35,27 @@
 			features: []
 		};
 	</script>
+	
+	<script>
+		$(document).ready(
+  			$('.popup').click(function(event) {
+  			  var width  = 575,
+  			      height = 400,
+  			      left   = ($(window).width()  - width)  / 2,
+  			      top    = ($(window).height() - height) / 2,
+  			      url    = this.href,
+  			      opts   = 'status=1' +
+  			               ',width='  + width  +
+  			               ',height=' + height +
+  			               ',top='    + top    +
+  			               ',left='   + left;
+  			  
+  			  window.open(url, 'twitter', opts);
+  			
+  			  return false;
+  			});
+  		);
+	</script>
 
 </head>
 
@@ -62,7 +83,7 @@
 		
 		<div class="connect">
 			<a href="" class="ss-icon">Facebook</a>
-			<a href="" class="ss-icon">Twitter</a>
+			<a class="twitter popup ss-icon" href="http://twitter.com/share?text=This%20is%20so%20easy">Twitter</a>
 			<a href="" class="ss-icon">Tumblr</a>
 			<a href="" class="ss-icon">Pinterest</a>
 		</div>

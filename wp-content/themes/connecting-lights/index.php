32406106c0a288078dcb819641e7a01e7abe8a7b
tyler madsen <tylrmden@gmail.com>
2012-08-03 16:54:17 -0400

    blog reversion

--- wp-content/themes/connecting-lights/index.php
+++ wp-content/themes/connecting-lights/index.php
@@ -13,28 +13,18 @@ get_header();
 				<h1><?php wp_title(''); ?></h1>
 			</header>
 			
-			<div>
+			<div class="detail-block">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
 				
-				<article>
+				<!-- <article> -->
 	
-					<header<?php if (! get_the_excerpt() && ! is_singular() ) { ?> class="full"<?php } ?>>
-						<a href="<?php the_permalink(); ?>">
-							<?php if (! is_singular() ) { ?><h1><?php the_title(); ?></h1><?php } ?>
-							<?php if ( get_the_excerpt() ) { 
-									the_post_thumbnail("ptmb");
-								} else {
-									the_post_thumbnail("full");
-								} 
-							?>
-						</a>
-					</header>
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 		
-					<div class="content">
-						<?php if (! is_singular() ) { the_excerpt(); } else { the_content(); } ?>
+					<div class="details">
+						<?php the_content(); ?>
 					</div>
 					
-				</article>
+				<!-- </article> -->
 			
 			<?php endwhile; else: ?>
 			<?php endif; ?>
@@ -53,14 +43,18 @@ get_header();
 				<?php
 
 				} else {
-
-				?>
-
-					<div class="alignleft"><?php next_post_link( '%link', '&laquo; %title'); ?></div>
-					<div class="alignright"><?php previous_post_link( '%link', '%title &raquo;'); ?></div>
-
-				<?php
-
+				
+					$prev_post = get_previous_post();
+					$next_post = get_next_post();
+					
+					if (! empty( $prev_post ) ) {
+						echo '<a class="alignleft" href="'. $prev_post->guid .'">&laquo; '. $prev_post->post_title .'</a>';
+					}
+					
+					if (! empty( $next_post ) ) {
+						echo '<a class="alignright" href="'. $next_post->guid .'">'. $next_post->post_title .' &raquo;</a>';
+					}
+					
 				}
 				
 			?>

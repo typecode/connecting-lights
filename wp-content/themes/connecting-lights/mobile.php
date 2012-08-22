8d0bd158b5ac90103dcf53ef3bbcb1d91d5a4d22
tyler madsen <tylrmden@gmail.com>
2012-08-22 17:24:14 -0400

    mobile work

--- wp-content/themes/connecting-lights/mobile.php
+++ wp-content/themes/connecting-lights/mobile.php
@@ -21,8 +21,6 @@ get_header();
 					<div>
 		
 						<p><strong>Connecting Light</strong> is a digital art installation along Hadrian’s Wall World Heritage Site. The installation consists of hundreds of large-scale, light-filled balloons transmitting colors from one-to-another, creating a communication network spanning over seventy miles.</p>
-						<p>Audience members are invited to participate by sending personalized messages along the light-lined wall at a number of viewing locations or, this Web site and companion mobile app.</p>
-						<p>Connecting Light investigates borders, imagining them not as a line of division, but as a source of connection.</p>
 						<p>The installation is open to the public from Friday, August 31st to Saturday, September 1st.</p>
 					
 					</div>

e2e9a60c56e62897e87a451938e6ee6dbb135969
tyler madsen <tylrmden@gmail.com>
2012-08-27 13:27:16 -0400

    404

---
+++ wp-content/themes/connecting-lights/404.php
@@ -0,0 +1,23 @@
+<?php 
+
+	get_header();
+
+?>
+
+		<div class="inverted">
+			<div class="inner">
+			
+				<header>
+					<h1>404 Error</h1>
+				</header>
+
+				<p>The page you're looking for cannot be found.</p>
+
+			</div>
+		</div>
+
+<?php
+
+	get_footer();
+
+?>
